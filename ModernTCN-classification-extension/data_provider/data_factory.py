00258016b157b2f7c0858063c36213195ff434df
onderakacik <oakacik18@ku.edu.tr>
2025-02-18 22:25:00 +0000

    extend classification tasks with physionet

---
+++ ModernTCN-classification-extension/data_provider/data_factory.py
@@ -0,0 +1,117 @@
+from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
+    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader, PhysioNetLoader
+from data_provider.uea import collate_fn
+from torch.utils.data import DataLoader
+import torch
+
+data_dict = {
+    'ETTh1': Dataset_ETT_hour,
+    'ETTh2': Dataset_ETT_hour,
+    'ETTm1': Dataset_ETT_minute,
+    'ETTm2': Dataset_ETT_minute,
+    'custom': Dataset_Custom,
+    'm4': Dataset_M4,
+    'PSM': PSMSegLoader,
+    'MSL': MSLSegLoader,
+    'SMAP': SMAPSegLoader,
+    'SMD': SMDSegLoader,
+    'SWAT': SWATSegLoader,
+    'UEA': UEAloader,
+    'PhysioNet': PhysioNetLoader
+}
+
+
+def physionet_collate_fn(data, max_len=None):
+    # Unpack all three values
+    features, labels, masks = zip(*data)
+    
+    # Convert to tensor and stack
+    features = torch.stack(features)  # Shape: [batch_size, 75, 72]
+    labels = torch.stack(labels)
+    masks = torch.stack(masks)
+    
+    # Transpose features to [batch_size, 75, 72] if needed
+    features = features.permute(0, 2, 1)  # Change to [batch_size, seq_len, features]
+    
+    return features, labels, masks
+
+
+def data_provider(args, flag):
+    Data = data_dict[args.data]
+    timeenc = 0 if args.embed != 'timeF' else 1
+
+    if flag == 'test':
+        shuffle_flag = False
+        # drop_last = True # This might effect the result so we set it to False
+        drop_last = False
+        if args.task_name == 'anomaly_detection' or args.task_name == 'classification':
+            batch_size = args.batch_size
+        else:
+            batch_size = 1  # bsz=1 for evaluation
+        freq = args.freq
+    else:
+        shuffle_flag = True
+        drop_last = True # This would not effect the reported results as it's not for the test set
+        batch_size = args.batch_size  # bsz for train and valid
+        freq = args.freq
+
+    if args.task_name == 'anomaly_detection':
+        drop_last = False
+        data_set = Data(
+            root_path=args.root_path,
+            win_size=args.seq_len,
+            flag=flag,
+        )
+        print(flag, len(data_set))
+        data_loader = DataLoader(
+            data_set,
+            batch_size=batch_size,
+            shuffle=shuffle_flag,
+            num_workers=args.num_workers,
+            drop_last=drop_last)
+        return data_set, data_loader
+    elif args.task_name == 'classification':
+        drop_last = False
+        data_set = Data(
+            root_path=args.root_path,
+            flag=flag,
+        )
+
+        if args.data == 'PhysioNet':
+            collate = physionet_collate_fn
+        else:
+            collate = collate_fn
+
+        data_loader = DataLoader(
+            data_set,
+            batch_size=batch_size,
+            shuffle=shuffle_flag,
+            num_workers=args.num_workers,
+            drop_last=drop_last,
+            collate_fn=lambda x: collate(x, max_len=args.seq_len)
+        )
+        return data_set, data_loader
+    else:
+        if args.data == 'm4':
+            drop_last = False
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            features=args.features,
+            target=args.target,
+            timeenc=timeenc,
+            freq=freq,
+            seasonal_patterns=args.seasonal_patterns
+        )
+        print(flag, len(data_set))
+        data_loader = DataLoader(
+            data_set,
+            batch_size=batch_size,
+            shuffle=shuffle_flag,
+            num_workers=args.num_workers,
+            drop_last=drop_last)
+
+        print(f"drop_last_bug after: {flag} {drop_last}")
+        return data_set, data_loader
