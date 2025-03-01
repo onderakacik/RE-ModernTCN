88aa001a5f59adbee43f18f476ae9676d18e7d11
onder.akacik <oakacik18@ku.edu.tr>
2025-03-01 15:55:41 +0100

    add scripts for extended experiments

---
+++ ModernTCN-classification-extension/scripts/speech_mfcc.sh
@@ -0,0 +1,32 @@
+experiment_name=Final_S_ks_3
+use_mfcc=True
+seq_len=161
+enc_in=20
+dims=32 # DIM = min(max(2 ** math.ceil(math.log2(ENC_IN)), d_min), d_max) => d_min = 32, d_max = 512
+
+
+# KS 13, 3 blocks MFCC
+python -u run.py --model_id Speech_Final_L13 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 --large_size 13 13 13 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
+
+# KS 31, 3 blocks MFCC
+python -u run.py --model_id Speech_Final_L31 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 --large_size 31 29 27 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
+
+# KS 51, 3 blocks MFCC
+python -u run.py --model_id Speech_Final_L51 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 --large_size 51 49 47 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
+
+# KS 71, 3 blocks MFCC
+python -u run.py --model_id Speech_Final_L71 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 --large_size 71 69 67 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
+
+
+
+# KS 13, 2 blocks MFCC
+python -u run.py --model_id Speech_Final_L13 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 13 13 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
+
+# KS 31, 2 blocks MFCC
+python -u run.py --model_id Speech_Final_L31 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 31 29 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
+
+# KS 51, 2 blocks MFCC
+python -u run.py --model_id Speech_Final_L51 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 51 49 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5 
+
+# KS 71, 2 blocks MFCC
+python -u run.py --model_id Speech_Final_L71 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 71 69 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5 
--- ModernTCN-short-term-extension/scripts/ETTh1.sh
+++ ModernTCN-short-term-extension/scripts/ETTh1.sh
@@ -1,7 +1,31 @@
-python -u run.py --is_training 1 --model_id ETTh1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 336 --pred_len 96 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 51 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --itr 5 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --des Exp --lradj type3 --use_multi_scale False --small_kernel_merged False
+# Prediction Length 6
+# First variation: seq_len = 12
+python -u run.py --des ETTh1_pl6 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 12 --pred_len 6 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 7 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
 
-python -u run.py --is_training 1 --model_id ETTh1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 336 --pred_len 192 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 51 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --itr 5 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --des Exp --lradj type3 --use_multi_scale False --small_kernel_merged False
+# Prediction Length 6
+# Second variation: seq_len = 18
+python -u run.py --des ETTh1_pl6 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 18 --pred_len 6 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 13 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
 
-python -u run.py --is_training 1 --model_id ETTh1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 336 --pred_len 336 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 51 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --itr 5 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --des Exp --lradj type3 --use_multi_scale False --small_kernel_merged False
+# Prediction Length 6
+# Third variation: seq_len = 24
+python -u run.py --des ETTh1_pl6 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 24 --pred_len 6 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 13 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
 
-python -u run.py --is_training 1 --model_id ETTh1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 336 --pred_len 720 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 51 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --itr 5 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --des Exp --lradj type3 --use_multi_scale False --small_kernel_merged False
+# Prediction Length 12
+# First variation: seq_len = 24
+python -u run.py --des ETTh1_pl12 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 24 --pred_len 12 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 13 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
+
+# Second variation: seq_len = 36
+python -u run.py --des ETTh1_pl12 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 36 --pred_len 12 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 13 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
+
+# Third variation: seq_len = 48
+python -u run.py --des ETTh1_pl12 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 48 --pred_len 12 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 13 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
+
+# Prediction Length 18
+# First variation: seq_len = 36
+python -u run.py --des ETTh1_pl18 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 36 --pred_len 18 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 13 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
+
+# Second variation: seq_len = 54
+python -u run.py --des ETTh1_pl18 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 54 --pred_len 18 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 13 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
+
+# Third variation: seq_len = 72
+python -u run.py --des ETTh1_pl18 --model_id ETTh1_True --is_training 1 --model ModernTCN --root_path ./all_six_datasets/ETT-small --data_path ETTh1.csv --data ETTh1 --features M --seq_len 72 --pred_len 18 --ffn_ratio 1 --patch_size 8 --patch_stride 4 --num_blocks 1 --large_size 13 --small_size 5 --dims 64 64 64 64 --head_dropout 0.0 --enc_in 7 --dropout 0.3 --train_epochs 100 --batch_size 512 --patience 20 --learning_rate 0.0001 --lradj type3 --use_multi_scale False --small_kernel_merged False --label_len 0 --itr 5
