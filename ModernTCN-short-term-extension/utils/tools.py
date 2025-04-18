81c8f36425610d34794d5ee98da6495274d6c8a1
onder.akacik <oakacik18@ku.edu.tr>
2025-04-18 22:34:41 +0200

    fix tolerance

--- ModernTCN-short-term-extension/utils/tools.py
+++ ModernTCN-short-term-extension/utils/tools.py
@@ -38,7 +38,7 @@ def adjust_learning_rate(optimizer, scheduler, epoch, args, printout=True):
 
 
 class EarlyStopping:
-    def __init__(self, patience=7, verbose=False, delta=0):
+    def __init__(self, patience=7, verbose=False, delta=1e-5):
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
