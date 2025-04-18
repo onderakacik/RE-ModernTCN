9152d0e96dfa81a99e26505786df983ae7b09f10
onder.akacik <oakacik18@ku.edu.tr>
2025-04-18 22:33:23 +0200

    add sh for extended

--- ModernTCN-classification-extension/scripts/physionet.sh
+++ ModernTCN-classification-extension/scripts/physionet.sh
@@ -1,14 +1,47 @@
-
-experiment_name=Final_PN_ks_2
+# MODERNTCN, OPTIMAL:
+# 4 layers, lradj type 3, dims 32, kernel 31
+experiment_name=PN_MTCN_Tuned
 seq_len=72
 enc_in=75
-dims=128 # DIM = min(max(2 ** math.ceil(math.log2(ENC_IN)), d_min), d_max) => d_min = 32, d_max = 512
+dims=32
+
+python -u run.py --model_id PN_MTCN --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 1 --large_size 31 29 27 25 --small_size 5 5 5 5 --dims $dims $dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --lradj type3 --itr 5
 
 
-python -u run.py --model_id PhysioNet_Final_L13 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 13 13 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --itr 5
+# MODERNTCN, ALL VAR. INDEPENDENT:
+# 4 layers, lradj type 3, dims 32, kernel 31, use_convffn2 False
+experiment_name=PN_MTCN_Tuned
+seq_len=72
+enc_in=75
+dims=32
+python -u run.py --model_id PN_MTCN_var_indep --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 1 --large_size 31 29 27 25 --small_size 5 5 5 5 --dims $dims $dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --lradj type3 --use_convffn2 False --itr 5
 
-python -u run.py --model_id PhysioNet_Final_L31 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 31 29 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --itr 5
 
-python -u run.py --model_id PhysioNet_Final_L51 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 51 49 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --itr 5 
+# MODERNTCN, EMBEDDING VAR. DEPENDENT (MIXED VARIABLES):
+# 4 layers, lradj type 3, dims 32, kernel 31
+experiment_name=PN_MTCN_Tuned
+seq_len=72
+enc_in=75
+dims=32
 
-python -u run.py --model_id PhysioNet_Final_L71 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 71 69 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --itr 5
+python -u run.py --model_id PN_MTCN_embedding_var_dep --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 1 --large_size 31 29 27 25 --small_size 5 5 5 5 --dims $dims $dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --lradj type3 --mix True --itr 5
+
+
+# CKCONV+MODERNTCN, EMBEDDING VAR. DEPENDENT (MIXED VARIABLES):
+# 4 layers, lradj type 3, dims 32, kernel 31
+experiment_name=PN_MTCN_Tuned
+seq_len=72
+enc_in=75
+dims=16
+
+python -u run.py --model_id PN_CKConv_MTCN_embedding_var_dep --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 1 --large_size 31 29 27 25 --small_size 5 5 5 5 --dims $dims $dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --lradj type3  --mix True --conv_type CKConv --causal True --separable False --itr 5
+
+
+# CKCONV+MODERNTCN, EMBEDDING VAR. INDEPENDENT (NO MIXING):
+# 4 layer, lradj type 3, dims 32, kernel 13
+experiment_name=PN_MTCN_Tuned
+seq_len=72
+enc_in=75
+dims=32
+
+python -u run.py --model_id PN_CKConv_MTCN_optimal --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/sepsis --data PhysioNet --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 1 --large_size 13 13 13 13 --small_size 5 5 5 5 --dims $dims $dims $dims $dims --head_dropout 0.0 --dropout 0.3 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --lradj type3 --itr 5 --mix False --conv_type CKConv --causal True --separable False
--- ModernTCN-classification-extension/scripts/speech_raw.sh
+++ ModernTCN-classification-extension/scripts/speech_raw.sh
@@ -1,30 +1,7 @@
-experiment_name=Final_RS_ks_2        
+experiment_name=Optimal_Speech
 use_mfcc=False
 seq_len=16000
 enc_in=1
-dims=32
-
-# KS 13, 2 blocks
-python -u run.py --model_id Raw_Speech_Final_L13 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 13 13 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
-
-# KS 13, 3 blocks
-python -u run.py --model_id Raw_Speech_Final_L13 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 --large_size 13 13 13 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
-
-# KS 31, 2 blocks
-python -u run.py --model_id Raw_Speech_Final_L31 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 31 29 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
-
-# KS 31, 3 blocks
-python -u run.py --model_id Raw_Speech_Final_L31 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 --large_size 31 29 27 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
-
-# KS 51, 2 blocks
-python -u run.py --model_id Raw_Speech_Final_L51 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 51 49 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
-
-# KS 51, 3 blocks
-python -u run.py --model_id Raw_Speech_Final_L51 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 --large_size 51 49 47 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
-
-# KS 71, 2 blocks
-python -u run.py --model_id Raw_Speech_Final_L71 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 --large_size 71 69 --small_size 5 5 --dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
-
-# KS 71, 3 blocks
-python -u run.py --model_id Raw_Speech_Final_L71 --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 1 --patch_stride 1 --num_blocks 1 1 1 --large_size 71 69 67 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --mfcc $use_mfcc --itr 5
-
+dims=512
+# KS 71, 3 blocks ==> Optimal
+python -u run.py --model_id Raw_Optimal --des $experiment_name --task_name classification --is_training 1 --root_path ./all_datasets/speech --data SpeechCommands --model ModernTCN --seq_len $seq_len --enc_in $enc_in --ffn_ratio 1 --patch_size 32 --patch_stride 16 --num_blocks 1 1 1 --large_size 71 69 67 --small_size 5 5 5 --dims $dims $dims $dims --head_dropout 0.0 --dropout 0.1 --class_dropout 0.1 --learning_rate 0.001 --batch_size 32 --train_epochs 100 --patience 10 --use_multi_scale False --lradj type3 --mfcc $use_mfcc --itr 5
